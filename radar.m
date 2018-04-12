bc2453a9c78ff3481d1a82801a43528e9b051849
Dylan Sewell <dylan.sewell94@hotmail.com>
2018-04-11 19:06:09 -0500

    Comit of SNR calculating Radar with test file

--- radar.m
+++ radar.m
@@ -23,25 +23,35 @@
 % Author: Dylan Sewell - dks153
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function radar(m, theta, radar_pos, gam, p_t, g, f, noise_fig, L, B)
+function radar(m, radar_pos, target_pos, target_vel, gam, p_t, g, f, noise_fig, L, B, Steptime, endTime)
 
     range1 = []; %Ranges to target given by the wall function
+    SNR = [];
+    spin_f = 200; %The speed of rotation of the beam in Hz
+    c = 2.998e8; %Speed of light in m/s
+    lambda = c/f; %The wavelength of the signal
+    sigma = 1; %Target RCS in m^2 for a human
+    k = 1.38064852e-23; %Boltzmaan's Constant
+    To = 290; %Standard Temp in Kelvin
     
-    for i = 1:1:theta
+    for time = 0:Steptime:endTime
+        target_pos(1) = target_vel * Steptime + target_pos(1);
+        theta = mod((4*spin_f*time*(180/pi)), 90);
         for j = 1:1:m
-            %Get range to target for all values of theta and break out of m
-            %      loop and continue through theta if nonzero range
-           range1 = [range1, wall(radar_pos, i, gam, j, 1, 1)];
+           %Get range to target for all values of theta and break out of m
+           %      loop and continue through theta if nonzero range
+           range1 = wall(radar_pos, theta, j, target_pos, 1, 1, 1);
            
            %Check to see if target was hit at this angle with j number of
            %bounces
-           if range1(end)
+           if range1
                break;
            end
         end
+        SNR = [SNR, (((p_t * (g^2) * (lambda^2) * sigma)/((4*pi)^3 *...
+            (range1^4) * k * To * B * noise_fig * L))*gam^j)];
+    end
     end
     
     % Calculate SNR for all range values. If range = 0, assume SNR = 0
     % (natural units)
-
-end
--- test.m
+++ test.m
@@ -7,13 +7,16 @@ clc;
 % end
 
 f = 1;
-gam = 1;
+gam = 0.5;
 noise_fig = 1;
 L = 1;
 B = 1;
 g = 1;
 p_t = 1;
 radar_pos = [1,1];
-m = 1;
-theta = 60;
-radar(m, theta, radar_pos, gam, p_t, g, f, noise_fig, L, B);
+target_pos = [6,6];
+target_vel = 1;
+m = 3;
+steptime = 0.1;
+endtime = 5;
+radar(m, radar_pos, target_pos, target_vel, gam, p_t, g, f, noise_fig, L, B, steptime, endtime);
